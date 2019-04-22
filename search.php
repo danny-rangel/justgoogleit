bfde563c8ebacbfd97e8d20ac85143746a1c8d9b
Danny Rangel <danielrangel.tech@gmail.com>
2019-04-21 23:27:42 -0700

    initial commit

---
+++ search.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Search Engine</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+
+            <div class="header">
+
+                <div class="headerContent">
+
+                    <div class="logoContainer">
+                        <a href="index.php">
+                            <img src="assets/images/logo.png" alt="logo">
+                        </a>
+                    </div>
+
+                    <div class="searchContainer">
+                        <form action="search.php" method="GET">
+                            <div class="searchBarContainer">
+                                <input class="searchBox" type="text" name="term">
+                                <button class="searchButton">
+                                    <img src="assets/images/icons/search.png" alt="searchIcon">
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                
+
+            </div>
+
+        </div>
+    </body>
+</html>
